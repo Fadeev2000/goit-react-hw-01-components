c7177fd0313198314005e268c073e57453fe5d99
Fadeev <fda2000@ua.fm>
2021-07-03 09:11:28 +0300

    Выполнил задание 4 без стилизации

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 //import logo from './logo.svg';
 import './App.css';
+
 import user from './data/user.json';
 import statisticalData from './data/statistical-data.json';
 import friends from './data/friends.json';
+import transactions from './data/transactions.json';
+
 import Profile from './components/Profile/Profile';
 import Statistics from './components/Statistics/Statistics';
 import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
 console.log('user', user);
 
@@ -19,12 +23,14 @@ function App() {
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
-      />
+      />;
 
-      <Statistics title="Upload stats" stats={statisticalData} />
-      <Statistics stats={statisticalData} />
+      <Statistics title="Upload stats" stats={statisticalData} />;
+      <Statistics stats={statisticalData} />;
 
-      <FriendList friends={friends} />
+      <FriendList friends={friends} />;
+
+      <TransactionHistory items={transactions} />;
 
     </>
   );
---
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import TransactionHistoryItem from '../TransactionHistoryItem/TransactionHistoryItem'
+
+function TransactionHistory({ items }) {
+    return (
+        <table class="transaction-history">
+            <thead>
+                <tr>
+                    <th>Type</th>
+                    <th>Amount</th>
+                    <th>Currency</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {items.map(({id, type, amount, currency}) => (
+                    <tr key = {id}>
+                        <TransactionHistoryItem type={type} amount={amount} currency={currency}/>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+}
+ 
+TransactionHistory.propTypes = {
+    items: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired
+        })
+    ).isRequired
+};
+
+export default TransactionHistory;
