75eb42d213750b8a5cb123cf2dd6a94209f8bb71
Fadeev <fda2000@ua.fm>
2021-07-04 20:41:52 +0300

    Выполнил стилизацию первого задания

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
+import css from './Profile.module.css'
 import defaultImg from '../../user.png'
 
+console.dir('css', css);
 function Profile({
     name,
     avatar,
@@ -9,30 +11,30 @@ function Profile({
     stats,
 }) {
   return (
-    <div class="profile">
-      <div class="description">
+    <div className={css.profile}>
+      <div className={css.description}>
         <img
           src={avatar}
           alt="Аватар пользователя"
-          class="avatar"
+          className={css.avatar}
         />
-        <p class="name">{name}</p>
-        <p class="tag">@{tag}</p>
-        <p class="location">{location}</p>
+        <p className={css.name}>{name}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
       </div>
 
-      <ul class="stats">
+      <ul className={css.stats}>
         <li>
-          <span class="label">Followers</span>
-          <span class="quantity">{stats.followers}</span>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{stats.followers}</span>
         </li>
         <li>
-          <span class="label">Views</span>
-          <span class="quantity">{stats.views}</span>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{stats.views}</span>
         </li>
         <li>
-          <span class="label">Likes</span>
-          <span class="quantity">{stats.likes}</span>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
